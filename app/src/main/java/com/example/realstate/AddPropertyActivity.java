3d0a80001de74cbf9717e4ebf4f22b56c84ad1da
Aria rouhani <aria.rouhan@gmail.com>
2019-08-01 20:55:55 +0430

    aria

--- app/src/main/java/com/example/realstate/AddPropertyActivity.java
+++ app/src/main/java/com/example/realstate/AddPropertyActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class AddPropertyActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
