064497af04fcc84c204f01f90a7c63646103b690
Aria rouhani <aria.rouhan@gmail.com>
2019-08-03 10:55:18 +0430

    Addlocation Button pass to maps activity

--- app/src/main/java/com/example/realstate/AddPropertyActivity.java
+++ app/src/main/java/com/example/realstate/AddPropertyActivity.java
@@ -2,15 +2,27 @@ package com.example.realstate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddPropertyActivity extends AppCompatActivity {
 
+    EditText editTextTitle ;
+    EditText editTextDescription ;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_property);
 
+        }
+    public void passToMap(View view){
+        Intent startNewActivity = new Intent(this , MapsActivity.class);
+        startActivity(startNewActivity);
     }
-}
+    }
+
