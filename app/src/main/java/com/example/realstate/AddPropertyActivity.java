df83f7b8e327fefd1ce1d3675849039303c7b46f
Parsa Mahmoodi <m.n.aparsam@gmail.com>
2019-08-04 12:57:33 +0430

    Added getting an image from gallery

--- app/src/main/java/com/example/realstate/AddPropertyActivity.java
+++ app/src/main/java/com/example/realstate/AddPropertyActivity.java
@@ -1,13 +1,34 @@
 package com.example.realstate;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.realstate.models.House;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class AddPropertyActivity extends AppCompatActivity {
 
@@ -15,8 +36,33 @@ public class AddPropertyActivity extends AppCompatActivity {
     EditText editTextDescription;
     Button button;
     int mode = 1;
+    ImageView imageView;
+    private static final int SELECTED_IMAGE = 1;
 
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case SELECTED_IMAGE:
+                if(resultCode == RESULT_OK){
+                    Uri uri = data.getData();
+                    String[] p = {MediaStore.Images.Media.DATA};
+                    Cursor cursor = getContentResolver().query(uri, p, null, null, null);
+                    cursor.moveToFirst();
+                    int columnIndex = cursor.getColumnIndex(p[0]);
+                    String filePath = cursor.getString(columnIndex);
+                    cursor.close();
+
+                    Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+                    Drawable drawable = new BitmapDrawable(AddPropertyActivity.this.getResources(), bitmap);
+                    imageView.setBackground(drawable);
+                }
+                break;
+                default:
+                    break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +70,19 @@ public class AddPropertyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_property);
         init();
 
+        imageView.setOnClickListener(View -> {
+            try {
+                requestPermissionForReadExtertalStorage();
+            }catch (Exception e){
+                Toast.makeText(this, "Something bad happened", Toast.LENGTH_SHORT).show();
+            }
+            Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, SELECTED_IMAGE);
+        });
 
 
         button.setOnClickListener(view -> {
+
             String title = editTextTitle.getText().toString().trim();
             String description = editTextDescription.getText().toString().trim();
             switch (mode) {
@@ -58,7 +114,16 @@ public class AddPropertyActivity extends AppCompatActivity {
         button = findViewById(R.id.buttonAddLocation);
         editTextTitle = findViewById(R.id.editTextTitel);
         editTextDescription = findViewById(R.id.editTextDescription);
+        imageView = findViewById(R.id.imageViewAddProperty);
 
+    }
+    public void requestPermissionForReadExtertalStorage() throws Exception {
+        try {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0x3);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
 
 }
